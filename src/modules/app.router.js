dc4f60c453b8a2be6d0f9f00d03bf5e0354b4c08
Farha Dahman <farhajehad@gmail.com>
2023-12-26 15:27:23 +0200

    adding the Booking and TourPackage routers

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -1,6 +1,8 @@
 import connectionDB from "../db/connection.js";
 import { globalErrorHandler } from "../services/errorHandling.js";
 import authRouter from "./auth/auth.router.js";
+import tourPackageRouter from "./tourPackage/tourPackage.router.js";
+import bookingRouter from "./booking/booking.router.js";
 
 const initApp = (app, express) => {
   app.use(express.json());
@@ -9,7 +11,8 @@ const initApp = (app, express) => {
     return res.status(200).json({ message: "Welcome" });
   });
   app.use("/auth", authRouter);
-
+  app.use("/tourPackage", tourPackageRouter);
+  app.use("/booking", bookingRouter);
   app.get("*", (req, res) => {
     return res.status(500).json({ message: "Page not found" });
   });
